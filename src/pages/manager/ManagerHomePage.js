c42864e55ab6ab9581af41a6a8fcd9c180064bc3
Ibrahimoluc <ibrahimalperoluc@gmail.com>
2025-10-30 16:48:25 +0300

    .

--- src/pages/manager/ManagerHomePage.js
+++ src/pages/manager/ManagerHomePage.js
@@ -75,7 +75,7 @@ const ManagerHomePage = () => {
             <div className="bg-white p-4 rounded shadow m-4">
                 <h3 className="text-lg font-semibold mb-4">🏭 Workstation List</h3>
                 {workstations.length === 0 ? (
-                    <p>İş istasyonu bulunamadı.</p>
+                    <p>Aktif iş istasyonu bulunamadı.</p>
                 ) : (
                     <table className="w-full text-sm border">
                         <thead className="bg-gray-100">
